8fc99884029f99f0fa7be77bcca46d2b8f78ae8e
Ad Friedman <adien330@yahoo.com>
2019-03-06 19:20:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,5 +2,11 @@ def badge_maker(name)
   puts "Hello, my name is #{name}."
 end
 
-def batch_badge_creator()
-end 
+def batch_badge_creator(arr)
+  arrs = []
+  i = 0
+  while i < arr.length
+    arrs << "Hello, my name is #{arr[i]}"
+    i+=1
+  end
+end
